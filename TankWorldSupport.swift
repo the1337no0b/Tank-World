b6102b5f9b968e8fa3d1c35b7b57828972eb391a
Innovation1042 <williamxu9999@hotmail.com>
2017-05-24 12:27:36 -0400

    Update TankWorldSupport.swift

--- TankWorldSupport.swift
+++ TankWorldSupport.swift
@@ -1,3 +1,14 @@
+/*still needs *some* support functions:
+func randomizeGameObjects
+func findwinner
+Optional:
+func makeOffsetPosition
+func findFreeAdjacent
+func isGoodIndex
+func getRandomDirection
+func distance
+*/
+
 import Foundation
 import Glibc
 extension TankWorld
