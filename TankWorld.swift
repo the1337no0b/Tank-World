8150829601600e70c9c5d1a8e95295d28eed0e93
the1337no0b <joesangiolo@gmail.com>
2017-06-06 21:01:28 -0400

    Update TankWorld.swift

--- TankWorld.swift
+++ TankWorld.swift
@@ -1,55 +1,24 @@
 //complete*
 
-class TankWorld: CustomStringConvertible {
-
+class TankWorld {
+	var grid = [[gameObject?]] ()
+	var turn: Int
 	var topLine: String = "."
-	var bottomText: String = "|"
-	//let gridSize = 5
-	var gridID: [String] = Array(repeating: Array(repeating: 0, count: 5), count: 5)
-	var gridEnergy = Array(repeating: Array(repeating: 0, count: 5), count: 5)
-	var gridLocation = Array(repeating: Array(repeating: 0, count: 5), count: 5)
-
-	func array1Dboxes(array: [Int]) -> String {
-		var content: String = "|"
-		bottomText = "|"
-			for i in array {
-				if i == 0 {
-					content += "       |"
-				}
-				else {
-					content += "   \(i)   |"
-				}
-				bottomText += "_______|"
-			}
-		return content
+	var fullContent: String = ""
+	init() {
+		grid = Array(repeating: Array(repeating: nil, count: 15), count: 15)
+		turn = 1
 	}
-
-	func addTopLine() {
-		for _ in gridID {
-			topLine += "_______."
-		}
+	func addGameObject(GameObject: GameObject, position: Position) {
+		grid[position.row][position.col] = GameObject
 	}
-
-	//func addValues(valueArray: [Int])
-
-	func placeGameObjects(go: gameObject) {
-		let xCoor: Int = go.location[0]
-		let yCoor: Int = go.location[1]
-		//print(xCoor, yCoor)
-		gridID[xCoor][yCoor] = go.id
-		gridEnergy[xCoor][yCoor] = go.energy
-		//gridLocation[xCoor][yCoor] = go.location
+	func doTurn() {
+		print(display())
+		print(logger.log)
+		nextTurn()
 	}
-
-	var description: String {
-		addTopLine()
-		var resultGrid: String = "\(topLine)\n"
-		for row in 0..<gridID.count {
-			resultGrid += "\(array1Dboxes(array: gridID[row]))\n"
-			resultGrid += "\(array1Dboxes(array: gridEnergy[row]))\n"
-			resultGrid += "\(array1Dboxes(array: gridLocation[row]))\n"
-			resultGrid += "\(bottomText)\n"
-		}
-		return resultGrid
+	func nextTurn() {
+		turn += 1
+		logger.log = ""
 	}
 }
