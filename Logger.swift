7587084c101659f3bf21d586c30645ca216eea8e
Innovation1042 <williamxu9999@hotmail.com>
2017-06-11 22:41:24 -0400

    Create Logger.swift

--- Logger.swift
+++ Logger.swift
@@ -1,16 +1,11 @@
 struct Logger: CustomStringConvertible {
   var log: String = ""
-  let name: String
   mutating func addLog(_ GOType: gameObject, _ message: String) {
     log += "\(GOType): \(message)\n"
   }
   mutating func clearLog()
   {
     log = ""
-  }
-  init(name: String)
-  {
-    self.name = name
   }
   var description: String {
     return "\(log)"
